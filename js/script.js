1d04dc926cbe9738503c0470bc174d880c31b8d5
Robert Burnat <burnat.robertt@gmail.com>
2017-09-15 21:17:02 +0200

    Init

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-
-$(document).on('click', 'a', event => {
-    event.preventDefault();
-
-    $('html, body').animate({
-        scrollTop: $( $.attr(event.currentTarget, 'href') ).offset().top
-    }, 800);
+$(function() {
+	$('a').on('click', function(event) {
+		if (this.hash !== "") {
+		    event.preventDefault();
+		    var hash = this.hash;
+		    $('html, body').animate({
+			    scrollTop: $(hash).offset().top}, 
+			    800);
+		}
+	});
 });
