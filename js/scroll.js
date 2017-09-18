8459d5dd2f29aad31945a516ad4856acaa843e8b
Robert Burnat <burnat.robertt@gmail.com>
2017-09-18 12:07:54 +0200

    Add scroll effects

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ duration: 2000 });
+sr.reveal('#head', 70);
+sr.reveal('#about-container', 50);
+sr.reveal('#skills-fluid', 50);
+sr.reveal('#project', 50);
+sr.reveal('#footer-container', 50);
