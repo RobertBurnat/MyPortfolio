4c16bd63a4bd56a748b1ff80096fdffc655d41b2
Robert Burnat <burnat.robertt@gmail.com>
2017-09-18 17:26:33 +0200

    Fix

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,10 @@
 window.sr = ScrollReveal({ duration: 2000 });
 sr.reveal('#head', 70);
-sr.reveal('#about-container', 50);
-sr.reveal('#skills-fluid', 50);
+sr.reveal('.hero', 70);
+sr.reveal('.about', 50);
+sr.reveal('p', 50);
+sr.reveal('.icon', 50);
+sr.reveal('#header-skills', 50);
+sr.reveal('#portfoliero', 50);
 sr.reveal('#project', 50);
 sr.reveal('#footer-container', 50);
